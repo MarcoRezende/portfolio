9431f8b5af152272c4dee1a4cbdee8884b5b75d4
unknown <marcorezende@outlook.com.br>
2021-03-24 17:06:13 -0300

    add initial structure to the Contacts component

--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,9 +1,72 @@
+import { RiMapPinFill as MarkerIcon } from 'react-icons/ri';
+import { IoIosArrowDown as ArrowDownIcon } from 'react-icons/io';
+import { FaMapSigns as AddressIcon } from 'react-icons/fa';
+import { HiMail as ContactsIcon } from 'react-icons/hi';
+
+import {
+  Container,
+  Overlay,
+  PlaceholderImage,
+  Location,
+  Address,
+  AddressPrimary,
+  AddressAndContacts,
+  Item,
+  MapContainer,
+  Map,
+} from '../styles/components/Contacts';
+
 const Contacts: React.FC = () => {
-	return (
-		<aside>
-			<h1>Ola</h1>
-		</aside>
-	);
+  const sources = {
+    low: 'https://loremflickr.com/600/800/city',
+    medium: 'https://loremflickr.com/400/600/city',
+    high: 'https://loremflickr.com/200/4200/city',
+  };
+  return (
+    <Container>
+      <Overlay>
+        <PlaceholderImage sources={sources} />
+
+        <Location>
+          <Address>
+            <MarkerIcon />
+
+            <AddressPrimary>
+              <h2>State</h2>
+              <span>City</span>
+            </AddressPrimary>
+
+            <ArrowDownIcon />
+
+            <AddressAndContacts>
+              <Item>
+                <h2>
+                  <AddressIcon />
+                  Endereço
+                </h2>
+                <span>Street name, Nº1</span>
+                <span>CEP: 00000000</span>
+              </Item>
+
+              <Item>
+                <h2>
+                  <ContactsIcon />
+                  Contatos
+                </h2>
+                <span>marco@email</span>
+                <span>Tel.: (11) 4157-2960</span>
+                <span>Cel.: (11) 94632-1887</span>
+              </Item>
+            </AddressAndContacts>
+          </Address>
+
+          <MapContainer>
+            <Map />
+          </MapContainer>
+        </Location>
+      </Overlay>
+    </Container>
+  );
 };
 
 export default Contacts;
---
+++ src/styles/components/Contacts.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside``;
+
+export const Overlay = styled.div``;
+
+export const PlaceholderImage = styled.div``;
+
+export const Details = styled.div``;
+
+export const Location = styled.div``;
+
+export const Address = styled.div``;
+
+export const AddressPrimary = styled.div``;
+
+export const AddressAndContacts = styled.div``;
+
+export const Item = styled.div``;
+
+export const MapContainer = styled.div``;
+
+export const Map = styled.div``;
